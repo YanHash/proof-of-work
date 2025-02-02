08b1a40d5ee08da925a1a17e151564577a1da6c0
yanis <yanis.arab.contact@gmail.com>
2025-02-02 20:00:42 +0100

    mise à jour de isValidChain() avec des logs et l'import manquant

--- utils/chain.js
+++ utils/chain.js
@@ -9,13 +9,18 @@ var ChainUtilities = function ChainUtilities(){
   this.isValidChain = isValidChain;
 
   function isValidChain(chain){
+    if(chain.length === 0) {
+      return true;
+    }
     if(chain.length>0){
       for (var i = 1; i < chain.length; i++) {
-        var lastBlockHash = validator.calculateHash(chain[i-1]);
+        var lastBlockHash = validator.calculateHash(chain[i-1].transaction[0]);
         if(lastBlockHash !== chain[i].previousHash){
+          console.error(`Erreur de hash : attendu ${lastBlockHash}, trouvé ${chain[i].previousHash} au bloc ${chain[i].index}`);
           return false;
         }
-        if(validatorUtilities.generateProof(chain[i]) !== chain[i].proof){
+        if(validatorUtilities.generateProof(chain[i].transaction[0]) !== chain[i].proof){
+          console.error(`Erreur de preuve : attendu ${validatorUtilities.generateProof(chain[i].transaction[0])}, trouvé ${chain[i].proof} au bloc ${chain[i].index}`);
           return false;
         }
       }
