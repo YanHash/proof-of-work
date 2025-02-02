a0966d135e4628df172f92e757fe9ed1aabb2ea4
yanis <yanis.arab.contact@gmail.com>
2025-02-02 19:59:30 +0100

    correction de init() et newTransaction() en fonction de la logique de validator

--- model/blockchain.js
+++ model/blockchain.js
@@ -4,7 +4,7 @@ var ChainUtilities = require('../utils/chain');
 var validator = require('../utils/validator');
 
 
-var blockchain = function blockchain(){
+var blockchain = function blockchain() {
 
   var self = this;
 
@@ -18,43 +18,50 @@ var blockchain = function blockchain(){
   this.chain;
   this.currentTransactions;
 
-  function init(){
+  function init() {
     /*
     *  initialize the blockchain, creating a new empty chain,
     *  an empty transactions list and creating the first block
     */
     self.chain = [];
-    self.currentTransactions = [];
-    self.newBlock(100, 1);
+    // Modification du code car la PoW se basant sur les transaction elle ne sera jamais bonne
+    // pour les 2 premiers blocs
+    self.currentTransactions = [{
+      sender: "127.0.0.1",
+      receiver: "127.0.0.255",
+      amount: 1
+    }];
+    var proof = validator.generateProof(self.currentTransactions[0])
+    self.newBlock(proof, 1);
   }
 
-  function getChain(){
+  function getChain() {
     /*
     *  returns the chain
     */
     return self.chain;
   }
 
-  function mine(miner){
+  function mine(miner) {
     /*
     *  implements the mining function. simple as is, it just
     *  creates a new transaction with "sender" 0 to show that
     *  this is a mined block.
     */
 
-    var lastBlock = self.chain[self.chain.length-1];
-    var transaction = newTransaction(0,miner,1);
+    var lastBlock = self.chain[self.chain.length - 1];
+    var transaction = newTransaction("0", miner, 1);
     var proof = validator.generateProof(transaction);
     var previousHash = validator.calculateHash(lastBlock.transaction[0]);
     return newBlock(proof, previousHash);
   }
 
-  function newBlock(proof, previousHash){
+  function newBlock(proof, previousHash) {
     /*
     *  Generate a new blocks and adds it to the chain
     */
     var block = {
-      "index": self.chain.length+1,
+      "index": self.chain.length + 1,
       "timestamp": new Date().getTime(),
       "transaction": self.currentTransactions,
       "proof": proof,
@@ -65,7 +72,7 @@ var blockchain = function blockchain(){
     return block;
   }
 
-  function newTransaction(sender, receiver, amount){
+  function newTransaction(sender, receiver, amount) {
     /*
     *  Generate a new transaction
     */
@@ -75,37 +82,37 @@ var blockchain = function blockchain(){
       amount: amount
     };
     self.currentTransactions.push(transaction);
+
+    console.log(typeof self.chain[self.chain.length-1].transaction[0])
+
+    var proof = validator.generateProof(transaction);
+    var prevHash = validator.calculateHash(self.chain[self.chain.length-1].transaction[0]);
+    self.newBlock(proof, prevHash)
+
+    self.currentTransactions = [];
     return transaction;
   }
 
-  function checkChain(){
+  function checkChain() {
     /* Le hash précédent de chaque bloc correspond bien au hash du bloc précédent */
     var isValidChain = ChainUtilities.isValidChain(self.chain);
-    if (!isValidChain) { 
+    if (!isValidChain) {
       console.error("La chaîne globale est invalide.");
       return [];
     }
-    
+
     for (var i = 0; i < self.chain.length; i++) {
       const block = self.chain[i];
       /* La structure de chaque bloc est complète */
       if (
-        !block.index || 
-        !block.timestamp || 
-        !block.transaction || 
-        !block.proof || 
+        !block.index ||
+        !block.timestamp ||
+        !block.transaction ||
+        !block.proof ||
         !block.previousHash
-      ) { 
+      ) {
         console.error(`Structure du bloc ${block.index} invalide.`);
-        return []; 
-      }
-
-      /* La preuve (proof) de chaque bloc est valide */
-      var generatedProof = validator.generateProof(block.transaction[0]);
-      var givenProof = block.proof;
-      if (generatedProof != givenProof) { 
-        console.error(`La preuve du bloc ${block.index} est invalide.`);
-        return []; 
+        return [];
       }
 
       /* Les indices des blocs sont séquentiels */
@@ -113,14 +120,23 @@ var blockchain = function blockchain(){
         console.error(`Les indices ne sont pas séquentiels au bloc ${block.index}.`);
         return [];
       };
+
+      /* La preuve (proof) de chaque bloc est valide */
+      var generatedProof = validator.generateProof(block.transaction[0]);
+      var givenProof = block.proof;
+      console.log(`block : ${block.transaction} - generated Proof ${generatedProof} - Given Proof : ${givenProof}}`)
+      if (generatedProof != givenProof) {
+        console.error(`La preuve du bloc ${block.index} est invalide.`);
+        return [];
+      }
     }
-    
+
     /* Chaine valide */
     return self.chain;
   }
 
 
-  if(blockchain.caller != blockchain.getInstance){
+  if (blockchain.caller != blockchain.getInstance) {
     throw new Error("This object cannot be instanciated");
   }
 
@@ -128,11 +144,11 @@ var blockchain = function blockchain(){
 
 
 blockchain.instance = null;
-blockchain.getInstance = function(){
-	if(this.instance === null){
-		this.instance = new blockchain();
-	}
-	return this.instance;
+blockchain.getInstance = function () {
+  if (this.instance === null) {
+    this.instance = new blockchain();
+  }
+  return this.instance;
 };
 
 module.exports = blockchain.getInstance();
